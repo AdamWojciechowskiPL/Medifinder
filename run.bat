9e44b5177ea8f9d8735852b8934903e0c899be22
AdamWojciechowskiPL <ad.wojciechowski@gmail.com>
2025-07-28 23:23:21 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,12 @@
 @echo off
-start /B pythonw run.py
+:: ============================================================================
+:: :: Uruchamia aplikacje Medicover Finder w srodowisku wirtualnym
+:: :: Wersja zalecana - bezposrednie wywolanie interpretera.
+:: ============================================================================
+
+:: Bezposrednio uruchamiamy interpreter pythonw.exe z folderu venv,
+:: podajac mu nasz skrypt run.py jako argument.
+:: To gwarantuje, ze zawsze zostanie uzyte wlasciwe srodowisko,
+:: bez potrzeby aktywacji.
+
+start /B "Medicover App" "venv\Scripts\pythonw.exe" "run.py"
